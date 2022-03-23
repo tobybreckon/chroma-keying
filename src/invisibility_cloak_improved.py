d9d6af2db4ce1f7aaad24b00ec91c281ae70c7c6
Toby Breckon <>
2022-03-23 00:02:57 +0000

    pep8 compliance

--- src/invisibility_cloak_improved.py
+++ src/invisibility_cloak_improved.py
@@ -13,13 +13,14 @@ import numpy as np
 
 # define the range of hues to detect - set automatically using mouse
 
-lower_green = np.uint8(np.array([255,0,0]))
-upper_green = np.uint8(np.array([255,255,255]))
+lower_green = np.uint8(np.array([255, 0, 0]))
+upper_green = np.uint8(np.array([255, 255, 255]))
 
 #####################################################################
 
 # mouse callback function - activated on any mouse event (click, movement)
 # displays and sets Hue range based on right click location
+
 
 def mouse_callback(event, x, y, flags, param):
 
@@ -32,12 +33,12 @@ def mouse_callback(event, x, y, flags, param):
 
     if event == cv2.EVENT_LBUTTONDOWN:
         print("HSV colour @ position (%d,%d) = %s (bounds set with +/- 20)" %
-              (x, y, ', '.join(str(i) for i in image_HSV[y, x])))
+              (x, y, ', '.join(str(i) for i in image_hsv[y, x])))
 
         # set Hue bounds on the Hue with +/- 15 threshold on the range
 
-        upper_green[0] = image_HSV[y, x][0] + 20
-        lower_green[0] = image_HSV[y, x][0] - 20
+        upper_green[0] = image_hsv[y, x][0] + 20
+        lower_green[0] = image_hsv[y, x][0] - 20
 
         # set Saturation and Value to eliminate very dark, noisy image regions
 
@@ -48,8 +49,9 @@ def mouse_callback(event, x, y, flags, param):
 
     elif event == cv2.EVENT_RBUTTONDOWN:
 
-        lower_green = np.uint8(np.array([255,0,0]))
-        upper_green = np.uint8(np.array([255,255,255]))
+        lower_green = np.uint8(np.array([255, 0, 0]))
+        upper_green = np.uint8(np.array([255, 255, 255]))
+
 
 #####################################################################
 
@@ -87,47 +89,58 @@ while (keep_processing):
 
     # convert the RGB images to HSV
 
-    image_HSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     # create a foreground mask that identifies the pixels in the range of hues
 
-    foreground_mask = cv2.inRange(image_HSV, lower_green, upper_green)
+    foreground_mask = cv2.inRange(image_hsv, lower_green, upper_green)
 
     # perform morphological opening and dilation on the foreground mask
 
     foreground_mask_morphed = cv2.morphologyEx(foreground_mask,
-                                cv2.MORPH_OPEN, np.ones((3,3), np.uint8), iterations = 5)
+                                               cv2.MORPH_OPEN,
+                                               np.ones((3, 3), np.uint8),
+                                               iterations=5)
     foreground_mask_morphed = cv2.morphologyEx(foreground_mask_morphed,
-                                cv2.MORPH_DILATE, np.ones((3,3), np.uint8), iterations = 5)
+                                               cv2.MORPH_DILATE,
+                                               np.ones((3, 3), np.uint8),
+                                               iterations=5)
 
     # extract the set of contours around the foreground mask and then the
     # convex hull around that set of contours. Update the foreground mask with
     # the convex hull of all the pixels in the region
 
     contours, _ = cv2.findContours(foreground_mask_morphed,
-                                    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                                   cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     if (len(contours) > 0):
-        hull = cv2.convexHull(np.vstack(list(contours[i] for i in range(len(contours)))))
-        cv2.fillPoly(foreground_mask_morphed, [hull], (255,255,255))
+        hull = cv2.convexHull(np.vstack(list(contours[i]
+                              for i in range(len(contours)))))
+        cv2.fillPoly(foreground_mask_morphed, [hull], (255, 255, 255))
 
-    # logically invert the foreground mask to get the background mask using logical NOT
+    # logically invert the foreground mask to get the background mask via NOT
 
     background_mask = cv2.bitwise_not(foreground_mask_morphed)
 
     # cut out the sub-part of the stored background we need using logical AND
 
-    cloaking_fill = cv2.bitwise_and(background, background, mask = foreground_mask_morphed)
+    cloaking_fill = cv2.bitwise_and(background, background,
+                                    mask=foreground_mask_morphed)
 
     # construct 3-channel RGB feathered background mask for blending
 
-    foreground_mask_feathered = cv2.blur(foreground_mask_morphed,(15,15)) / 255.0
-    background_mask_feathered = cv2.blur(background_mask,(15,15)) / 255.0
-    background_mask_feathered = cv2.merge([background_mask_feathered, background_mask_feathered, background_mask_feathered])
-    foreground_mask_feathered = cv2.merge([foreground_mask_feathered, foreground_mask_feathered, foreground_mask_feathered])
+    foreground_mask_feathered = cv2.blur(foreground_mask_morphed, (15, 15)) / 255.0
+    background_mask_feathered = cv2.blur(background_mask, (15, 15)) / 255.0
+    background_mask_feathered = cv2.merge([background_mask_feathered,
+                                           background_mask_feathered,
+                                           background_mask_feathered])
+    foreground_mask_feathered = cv2.merge([foreground_mask_feathered,
+                                           foreground_mask_feathered,
+                                           foreground_mask_feathered])
 
     # combine current camera image with cloaked region via feathered blending
 
-    cloaked_image = ((background_mask_feathered * image) + (foreground_mask_feathered * background)).astype('uint8')
+    cloaked_image = ((background_mask_feathered * image) +
+                     (foreground_mask_feathered * background)).astype('uint8')
 
     # display image with cloaking present
 
