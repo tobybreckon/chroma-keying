b40d53f379b455a386a1594ba12c2da821b70454
tobybreckon <toby.breckon@durham.ac.uk>
2026-07-29 11:49:23 +0100

    remove erroneous hue scaling

--- src/hsv_viewer.py
+++ src/hsv_viewer.py
@@ -33,7 +33,7 @@ while (keep_processing):
     image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     hue = np.zeros(image_hsv.shape, dtype=np.uint8)
-    hue[:, :, 0] = np.uint8(image_hsv[:, :, 0] * (0.7))
+    hue[:, :, 0] = np.uint8(image_hsv[:, :, 0])
     hue[:, :, 1] = np.ones(image_hsv[:, :, 1].shape) * 255
     hue[:, :, 2] = np.ones(image_hsv[:, :, 2].shape) * 255
     colour_mapped_hue = cv2.cvtColor(hue, cv2.COLOR_HSV2RGB)  # RGB better
